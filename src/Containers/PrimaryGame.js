1da941258e71f35cdab6be87c4b3db4ab3622ad4
Meghana1603 <meghanamannem163@gmail.com>
2022-02-09 18:38:30 +0530

    added primary navbar

--- src/Containers/PrimaryGame.js
+++ src/Containers/PrimaryGame.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Animation from "../components/animation";
+import PrimaryNavbar from "../components/primaryNavbar";
 
 const PrimaryGame = () => {
   return (
     <div className="PGOuterDiv">
+      <PrimaryNavbar />
       <Animation />
     </div>
   );
