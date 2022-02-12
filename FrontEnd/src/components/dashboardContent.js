c30ab536053bbe8566bb002db4b9d2ad88d7b23a
NK Pranesh <nkpranesh@gmail.com>
2022-02-12 23:17:36 +0530

    adf

---
+++ FrontEnd/src/components/dashboardContent.js
@@ -0,0 +1,53 @@
+import React from "react";
+import "../stylesheets/dashboardContent.css";
+import SkaterGirl from "../media/Skater Girl.svg";
+import Student from "../media/Studentimg.svg";
+import Dashboard from "./dashboard";
+
+const DashboardContent = () => {
+  return (
+    <div className="DBContentDiv">
+      <div className="DBTopNav">Welcome Nagveer!!</div>
+      <div className="DBContent">
+        <div className="DBTestArea">
+          <span className="DBTestText">Take a test today.</span>
+          <div className="DBTestFIled">
+            <div className="DBStoryMode">
+              <img src={SkaterGirl} alt="SkaterGirl" />
+              <div className="DBstoryContent">
+                <p className="DBStoryheading">Story Mode</p>
+                <div className="DBStoryContentRight">
+                  <p className="DBStoryInfo">
+                    Track sales and customers on any device. Track sales and
+                    customers on any device.
+                  </p>
+                  <div className="DBStartButton">
+                    <button>Start Test</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="DBNormalMode">
+              <img src={Student} alt="" />
+              <div className="DBstoryContent">
+                <p className="DBStoryheading">Normal Mode</p>
+                <div className="DBStoryContentRight">
+                  <p className="DBStoryInfo">
+                    Track sales and customers on any device. Track sales and
+                    customers on any device.
+                  </p>
+                  <div className="DBStartButton">
+                    <button>Start Test</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <Dashboard />
+      </div>
+    </div>
+  );
+};
+
+export default DashboardContent;
