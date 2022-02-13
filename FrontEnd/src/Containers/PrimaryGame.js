e7740532d55fe4017a8aabe97923a97b12de9573
Meghana1603 <meghanamannem163@gmail.com>
2022-02-13 17:15:40 +0530

    Timer

--- FrontEnd/src/Containers/PrimaryGame.js
+++ FrontEnd/src/Containers/PrimaryGame.js
@@ -3,8 +3,13 @@ import Animation from "../components/animation";
 import QuestionBox from "../components/questionBox";
 import PrimaryNavbar from "../components/primaryNavbar";
 import "../stylesheets/PrimaryGame.css";
+import RedirectToHomePage from "../components/redirectToHomePage";
 
 const PrimaryGame = () => {
+  const [min,setMin]=useState(0);
+  const [sec,setSec]=useState(0);
+  let [questionNumber, setQuestionNumber] = useState(1);
+  const [score, setScore]= useState(0);
   let animate = () => {
     let frame = document.getElementById(
       "HomeAnimation" + popOut
@@ -28,11 +33,14 @@ const PrimaryGame = () => {
   };
 
   let [popOut, setPopOut] = useState(1);
+  const [over, setOver] = useState(false);
 
   return (
     <div className="PGOuterDiv">
-      <PrimaryNavbar />
+      <PrimaryNavbar questionNumber={questionNumber} score={score} min={min} sec={sec}setMin={setMin} setSec={setSec} over={over} setOver={setOver}/>
       <div className="PGMainDiv">
+      {!over ?
+      <>
         <div className="PGLeftDiv">
           <iframe
             className="Butterflies"
@@ -45,9 +53,19 @@ const PrimaryGame = () => {
             animate={animate}
             setPopOut={setPopOut}
             popOut={popOut}
+            setQuestionNumber={setQuestionNumber}
+            questionNumber={questionNumber}
+            setMin={setMin}
+            score={score}
+            setScore={setScore}
+            setSec={setSec} min={min} sec={sec}
+
+            
           />
         </div>
         <Animation popOut={popOut} />
+        </>
+        : <RedirectToHomePage/> }
       </div>
     </div>
   );
--- FrontEnd/src/components/questionBox.js
+++ FrontEnd/src/components/questionBox.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React,{useState} from "react";
 import "../stylesheets/questionBox.css";
-
+let flag=0;
+var time = [0,0,0,0,0,0,0,0,0,0];
 const QuestionBox = (props) => {
+  const [[prevMin,prevSec],setPrevTime]=useState([10,0]);
   let questions = [
     "Father Elephant is aged three times more than his son. After 8 years, he would be two and a half times of his son’s age. After further 8 years, how many times would he be of his son’s age?",
     "Father2 Elephant is aged three times more than his son. After 8 years, he would be two and a half times of his son’s age. After further 8 years, how many times would he be of his son’s age?",
     "Father3 Elephant is aged three times more than his son. After 8 years, he would be two and a half times of his son’s age. After further 8 years, how many times would he be of his son’s age?",
     "Father4 Elephant is aged three times more than his son. After 8 years, he would be two and a half times of his son’s age. After further 8 years, how many times would he be of his son’s age?",
     "Father5 Elephant is aged three times more than his son. After 8 years, he would be two and a half times of his son’s age. After further 8 years, how many times would he be of his son’s age?",
+    "Father6 Elephant is aged three times more than his son. After 8 years, he would be two and a half times of his son’s age. After further 8 years, how many times would he be of his son’s age?",
+    "Father7 Elephant is aged three times more than his son. After 8 years, he would be two and a half times of his son’s age. After further 8 years, how many times would he be of his son’s age?",
+    "Father8 Elephant is aged three times more than his son. After 8 years, he would be two and a half times of his son’s age. After further 8 years, how many times would he be of his son’s age?",
+    "Father9 Elephant is aged three times more than his son. After 8 years, he would be two and a half times of his son’s age. After further 8 years, how many times would he be of his son’s age?",
+    "Father10 Elephant is aged three times more than his son. After 8 years, he would be two and a half times of his son’s age. After further 8 years, how many times would he be of his son’s age?",
   ];
 
   let situations = [
@@ -16,15 +23,20 @@ const QuestionBox = (props) => {
     "Hey Nagveer3, meet John. John likes to explore undiscovered places ,so he decided to visit a hotel today. Unfortunately he couldn’t remember the name of the hotel. Help John in remembering the name of the hotel by answering the question mentioned below.",
     "Hey Nagveer4, meet John. John likes to explore undiscovered places ,so he decided to visit a hotel today. Unfortunately he couldn’t remember the name of the hotel. Help John in remembering the name of the hotel by answering the question mentioned below.",
     "Hey Nagveer5, meet John. John likes to explore undiscovered places ,so he decided to visit a hotel today. Unfortunately he couldn’t remember the name of the hotel. Help John in remembering the name of the hotel by answering the question mentioned below.",
+    "Hey Nagveer6, meet John. John likes to explore undiscovered places ,so he decided to visit a hotel today. Unfortunately he couldn’t remember the name of the hotel. Help John in remembering the name of the hotel by answering the question mentioned below.",
+    "Hey Nagveer7, meet John. John likes to explore undiscovered places ,so he decided to visit a hotel today. Unfortunately he couldn’t remember the name of the hotel. Help John in remembering the name of the hotel by answering the question mentioned below.",
+    "Hey Nagveer8, meet John. John likes to explore undiscovered places ,so he decided to visit a hotel today. Unfortunately he couldn’t remember the name of the hotel. Help John in remembering the name of the hotel by answering the question mentioned below.",
+    "Hey Nagveer9, meet John. John likes to explore undiscovered places ,so he decided to visit a hotel today. Unfortunately he couldn’t remember the name of the hotel. Help John in remembering the name of the hotel by answering the question mentioned below.",
+    "Hey Nagveer10, meet John. John likes to explore undiscovered places ,so he decided to visit a hotel today. Unfortunately he couldn’t remember the name of the hotel. Help John in remembering the name of the hotel by answering the question mentioned below.",
   ];
 
   return (
     <div className="QBMainDiv">
       <div className="QBSituationDiv">
-        <span>{situations[props.popOut - 1]}</span>
+        <span>{situations[props.questionNumber - 1]}</span>
       </div>
       <div className="QBQuestionDiv">
-        <div className="QBQuestion">{"Q. " + questions[props.popOut - 1]}</div>
+        <div className="QBQuestion">{"Q. " + questions[props.questionNumber - 1]}</div>
         <div className="QBOptions">
           <div className="QBOptionsTop">
             <span>
@@ -45,34 +57,62 @@ const QuestionBox = (props) => {
         </div>
       </div>
       <div className="QBButtonDiv">
-        <button
+        {props.questionNumber<10 ?
+          <button
+            className="QBNextButton"
+            onClick={() => {
+              time[props.questionNumber-1]=(prevMin*60+prevSec)-(props.min*60+props.sec);
+              setPrevTime([props.min,props.sec]);
+              console.log(time);
+              flag=flag+1;
+              if(flag%2==0){
+                  props.animate();
+                  let nextAnimation = props.popOut + 1;
+                  setTimeout(() => {
+                    let newAnimation = document.getElementsByClassName(
+                      "Animation" + nextAnimation
+                    )[0];
+                    newAnimation.classList.remove("popInLeft");
+                  }, 10001);
+                  setTimeout(() => {
+                    let animation = document.getElementsByClassName(
+                      "Animation" + props.popOut
+                    )[0];
+                    animation.classList.add("popOutRight");
+                  }, 4000);
+
+                  setTimeout(() => {
+                    document.getElementsByClassName(
+                      "Animation" + props.popOut
+                    )[0].style.display = "none";
+                  }, 7001);
+                  setTimeout(() => {
+                    props.setPopOut(props.popOut + 1);
+                    props.setQuestionNumber(props.questionNumber + 1);
+                    props.setScore(props.score + (props.min*60 + props.sec)*10);
+                  },4001);
+              }
+              else{
+                setTimeout(() => {
+                  props.setQuestionNumber(props.questionNumber + 1);
+                  props.setScore(props.score + (props.min*60 + props.sec)*10);
+                },1001);
+              }
+              
+              
+            }}
+          >
+            Next
+          </button>
+          : <button
           className="QBNextButton"
           onClick={() => {
-            props.animate();
-            let nextAnimation = props.popOut + 1;
-            setTimeout(() => {
-              let newAnimation = document.getElementsByClassName(
-                "Animation" + nextAnimation
-              )[0];
-              newAnimation.classList.remove("popInLeft");
-            }, 10001);
-            setTimeout(() => {
-              let animation = document.getElementsByClassName(
-                "Animation" + props.popOut
-              )[0];
-              animation.classList.add("popOutRight");
-            }, 4000);
 
-            setTimeout(() => {
-              document.getElementsByClassName(
-                "Animation" + props.popOut
-              )[0].style.display = "none";
-              props.setPopOut(props.popOut + 1);
-            }, 7001);
           }}
         >
-          Next
+          Submit
         </button>
+        }
       </div>
     </div>
   );
